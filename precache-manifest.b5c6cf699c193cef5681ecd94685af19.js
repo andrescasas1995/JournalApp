15453adc9c8d6ffcda338d43463881fb8e773ccc
Andres Casas <walterandrescasas@hotmail.com>
2020-08-23 00:09:18 -0500

    Updates

---
+++ precache-manifest.b5c6cf699c193cef5681ecd94685af19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04ada568442d293f8ca221f2218f2d5a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6aa6c5a31322b4e3d938",
+    "url": "./static/css/main.01388bf6.chunk.css"
+  },
+  {
+    "revision": "8a70b4d6888b9a627a05",
+    "url": "./static/js/2.407cf4bb.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "./static/js/2.407cf4bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aa6c5a31322b4e3d938",
+    "url": "./static/js/main.4a49753f.chunk.js"
+  },
+  {
+    "revision": "2df0d9c45dce27d773a3",
+    "url": "./static/js/runtime-main.09e30608.js"
+  }
+]);
